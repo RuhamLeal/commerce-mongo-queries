b35c882d44cf8342b19ef07a25a04d63e2972d9a
Ruham Leal <ruhamxlpro@hotmail.com>
2023-02-13 16:36:26 -0300

    feat: adding 1-10 queries challenges

---
+++ challenges/desafio2.js
@@ -0,0 +1,4 @@
+db
+  .produtos
+  .find({}, { _id: 0, vendidos: 1, nome: 1 })
+  .sort({ vendidos: 1 });
---
+++ challenges/desafio7.js
@@ -0,0 +1,6 @@
+db
+  .produtos
+  .find(
+    { vendidos: { $ne: 50 }, tags: { $exists: false } }, 
+    { _id: 0, vendidos: 1, nome: 1 },
+  );
